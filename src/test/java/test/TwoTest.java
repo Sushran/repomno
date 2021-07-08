e6efb773cb03633eebd23f4fb917a373a831b0a6
sushmitha <sush@gmail.com>
2021-07-08 13:42:32 +0530

    third commit

--- src/test/java/test/TwoTest.java
+++ src/test/java/test/TwoTest.java
@@ -12,7 +12,10 @@ public class TwoTest extends Base {
 	public WebDriver driver;
 @Test
 	public void testtwo() throws IOException, InterruptedException {
+	
 		System.out.println("rani has updated this code");
+		System.out.println("rani had updated second time ");
+		
 		System.out.println("testtwo");
 		 driver = intializeDriver();
 		driver.get("http://tutorialsninja.com/demo/");
